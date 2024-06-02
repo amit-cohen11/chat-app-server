7694b25fff4d88cf72d9ae798d6537957721cdb1
amit cohen <cohen415@walla.com>
2024-06-02 21:44:03 +0300

    added message routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db.js");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes.js");
 const chatRoutes = require("./routes/chatRoutes.js");
+const messageRoutes = require("./routes/messageRoute.js");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 
 const app = express();
@@ -16,17 +17,9 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-// app.get("/api/chat", (req, res) => {
-//   res.send(chats);
-// });
-
-// app.get("/api/chat/:id", (req, res) => {
-//   const singleChat = chats.find((c) => c._id === req.params.id);
-//   res.send(singleChat);
-// });
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
